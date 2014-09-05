9d0eddbcc71dff2ed749f3b2a9ff392a5b0e490f
T.A. de Jong <tadj42@gmail.com>
2014-09-05 15:38:28 +0200

    Maybe solve A

--- A.cpp
+++ A.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -10,6 +12,50 @@ int main()
 	
 	while (numEntries--)
 	{
+		int n;
+		cin >> n;
+		cin.ignore();
+
+		vector<int> genome2(n);
+
+		{
+			map<int, int> mapping;
+			int x;
+			for (int i = 0; i < n; i++)
+			{
+				cin >> x;
+				mapping[x] = i;
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				cin >> x;
+				genome2[i] = mapping[x];
+			}
+		}
+
+		bool space = false;
+		for (int start = 0; start < n; start++)
+		{
+			int i = genome2[start];
+			int max = i, length = 1;
+
+			while (i != start)
+			{
+				++length;
+				i = genome2[i];
+				if (i > max)
+					max = i;
+			}
+
+			if (max - start == length - 1)
+			{
+				cout << (space ? " " : "") << start+1 << "-" << max+1;
+				space = true;
+			}
+		}
+
+		cout << endl;
 	}
 	return 0;
 }
