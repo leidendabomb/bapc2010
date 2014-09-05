9517154d486dfc0f38dea83c1da9f8a01d5d1edb
Bert Peters <bert.ljpeters@gmail.com>
2014-09-05 15:37:47 +0200

    Greedy oplossing voor B. Werkt voor testdata.

--- B.cpp
+++ B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,37 @@ int main()
 	
 	while (numEntries--)
 	{
+		int n, m;
+		cin >> n >> m;
+		int a, b;
+		cin >> a >> b;
+		vector<int> singles(n);
+		for (int i = 0; i < n; i++) {
+			cin >> singles[i];
+		}
+		int minutes = 0;
+		int score = 0;
+		for (int i = 0; i < n; i++) {
+			if (minutes == m) {
+				minutes = 0;
+			}
+			if (singles[i] + minutes < m) {
+				minutes += singles[i];
+			} else {
+				int penaltyA = (singles[i] - (m - minutes)) * a;
+				int penaltyB = (m - minutes) * b;
+				if (penaltyA <= penaltyB) {
+					score += penaltyA;
+					minutes = 0;
+				} else {
+					score += penaltyB;
+					minutes = singles[i];
+				}
+			}
+		}
+		if (minutes)
+			score += (m - minutes) * b;
+		cout << score << endl;
 	}
 	return 0;
 }
