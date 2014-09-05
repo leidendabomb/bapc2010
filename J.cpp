bad9733d34080f5f2de84cbed121f52c47914a55
T.A. de Jong <tadj42@gmail.com>
2014-09-05 16:15:56 +0200

    Shit be broke, yo

--- J.cpp
+++ J.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
+
+struct word
+{
+	int x;
+	int y;
+	int length;
+	string word;
+
+	list<word*> conflicts;
+};
 
 int main()
 {
@@ -10,6 +21,39 @@ int main()
 	
 	while (numEntries--)
 	{
+		int H, V;
+		cin >> H >> V;
+
+		map<int, word> horizontal;
+		map<int, word> vertical;
+
+		for (int i = 0; i < H; i++)
+		{
+			int x, y;
+			cin >> x >> y;
+			horizontal[y].x = x;
+			horizontal[y].y = y;
+			cin >> horizontal[y].word;
+			horizontal[y].length = horizontal[y].word.size();
+		}
+
+		for (int i = 0; i < V; i++)
+		{
+			int x, y;
+			cin >> x >> y;
+			vertical[x].x = x;
+			vertical[x].y = y;
+			cin >> vertical[x].word;
+			vertical[x].length = vertical[x].word.size();
+
+			for (int y = y; y < vertical[x].y + vertical[x].length; y++)
+			{
+				if (horizontal.count(y))
+				{
+
+				}
+			}
+		}
 	}
 	return 0;
 }
